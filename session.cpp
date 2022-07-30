8fe5b1bc5a7bad174beb44b2edc5b65b19eee278
Frode Roxrud Gill <frodegill@gmail.com>
2022-07-30 17:10:03 +0200

    Generate ClientId

--- session.cpp
+++ session.cpp
@@ -1,6 +1,29 @@
 #include "session.h"
 
+#include <algorithm>
+#include <cmath>
+#include <random>
+
 
 void SessionManager::expireOldSessions()
 {
+}
+
+void SessionManager::generateClientId(std::string& client_id) const
+{
+  int required_id_length = std::min(static_cast<int>(::log10(m_session_map.size()+1)) + 2, 23);
+  constexpr int LEGAL_CLIENT_ID_CHARS_length = sizeof(LEGAL_CLIENT_ID_CHARS)/sizeof(LEGAL_CLIENT_ID_CHARS[0]) - 1;
+
+  std::random_device random_device;
+  std::default_random_engine random_engine(random_device());
+  std::uniform_int_distribution<int> random_provider(0, LEGAL_CLIENT_ID_CHARS_length);
+
+  do
+  {
+    client_id.clear();
+    for (int i=0; i<required_id_length; i++)
+    {
+      client_id += LEGAL_CLIENT_ID_CHARS[random_provider(random_engine)];
+    }
+  } while (m_session_map.contains(client_id));
 }
--- session.h
+++ session.h
@@ -1,6 +1,9 @@
 #ifndef _SESSION_H_
 #define _SESSION_H_
 
+#include <string>
+#include <unordered_map>
+
 
 class Session
 {
@@ -9,9 +12,17 @@ class Session
 
 class SessionManager
 {
+private:
+  static constexpr char LEGAL_CLIENT_ID_CHARS[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 public:
   void expireOldSessions();
+
+  void generateClientId(std::string& client_id) const;
+
+private:
+  std::unordered_map<std::string,Session> m_session_map;
+
 };
 
 #endif // _SESSION_H_
